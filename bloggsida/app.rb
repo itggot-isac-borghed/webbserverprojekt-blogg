d066c9c3432d90ce3010ee5686dd2d91c9a763a1
itggot-isac-borghed <35598522+itggot-isac-borghed@users.noreply.github.com>
2019-03-08 14:26:35 +0100

    Lagt till möjligheter att redigera och ta bort inlägg. Också gjort färdigt blogglistan.

--- bloggsida/app.rb
+++ bloggsida/app.rb
@@ -50,21 +50,65 @@ end
 get('/blogs') do
     db = SQLite3::Database.new("db/databas.db")
     db.results_as_hash = true
-    bloggar = db.execute('SELECT Titel,Ägare FROM Bloggar')
-    slim(:blogs, locals:{lista:bloggar})
+    blogs = db.execute('SELECT Titel,Ägare FROM Bloggar')
+    slim(:blogs, locals:{lista:blogs})
+end
+
+get('/blogs/:titel') do
+    db = SQLite3::Database.new("db/databas.db")
+    db.results_as_hash = true
+    blog = db.execute('SELECT Titel,Ägare,Id FROM Bloggar WHERE Titel=?', params["titel"])
+    if blog == []
+        redirect('/404')
+    end
+    inlägg = db.execute('SELECT Titel,Info,BildLänk FROM Inlägg WHERE BloggId=? ORDER BY Id DESC', blog[0]["Id"])
+    slim(:blog, locals:{lista:inlägg, blogg:blog})
 end
 
 get('/myblog') do
     db = SQLite3::Database.new("db/databas.db")
     db.results_as_hash = true
-    titel = db.execute('SELECT Titel FROM Bloggar WHERE Ägare=?', session[:account])
-    inlägg = []
-    if titel != []
-        id = db.execute('SELECT Id FROM Bloggar WHERE Ägare=?', session[:account])
-        session[:blog] = titel
-        inlägg = db.execute('SELECT Titel,Info FROM Inlägg WHERE BloggId=? ORDER BY Id DESC', id[0]["Id"])
+    if session[:account] != nil
+        blog = db.execute('SELECT Titel,Id FROM Bloggar WHERE Ägare=?', session[:account])
+        inlägg = []
+        if blog != []
+            session[:blog] = blog
+            inlägg = db.execute('SELECT Titel,Info,BildLänk,Id FROM Inlägg WHERE BloggId=? ORDER BY Id DESC', blog[0]["Id"])
+        end
+    else
+        redirect('/nologin')
     end
-    slim(:blog, locals:{lista:inlägg})
+    slim(:myblog, locals:{lista:inlägg})
+end
+
+get('/edit/:id') do
+    db = SQLite3::Database.new("db/databas.db")
+    db.results_as_hash = true
+    inlägg = db.execute('SELECT Titel,Info,BildLänk,Id,BloggId FROM Inlägg WHERE Id=? ORDER BY Id DESC', params["id"])
+    if session[:blog] == nil
+        session[:blog] = db.execute('SELECT Id FROM Bloggar WHERE Ägare=?', session[:account])
+        if session[:blog] == []
+            redirect('/noaccess')
+        end
+    end
+    if session[:blog][0]["Id"] != inlägg[0]["BloggId"]
+        redirect('/noaccess')
+    end
+    slim(:editpost, locals:{post:inlägg})
+end
+
+post('/edit/:id') do
+    db = SQLite3::Database.new("db/databas.db")
+    db.results_as_hash = true
+    db.execute('UPDATE Inlägg SET Titel=?,Info=?,BildLänk=? WHERE Id=?', params["Title"], params["Info"], params["Img"], params["id"])
+    redirect('/myblog')
+end
+
+post('/delete/:id') do
+    db = SQLite3::Database.new("db/databas.db")
+    db.results_as_hash = true
+    db.execute('DELETE FROM Inlägg WHERE Id=?', params["id"])
+    redirect('/myblog')
 end
 
 post('/createblog') do
@@ -87,5 +131,6 @@ end
 
 post('/logout') do
     session[:account] = nil
+    session[:blog] = nil
     redirect('/')
 end
